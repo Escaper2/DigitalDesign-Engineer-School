7ad2c367e0938978639efc079e770fc14b0f8d4b
Eskaper2 <vlad81552@gmail.com>
2023-05-27 21:22:00 +0300

    feat: second task

---
+++ TextAnalyzer/SecondTask/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+using TextProcessor;
+
+namespace SecondTask
+{
+    internal class Program
+    {
+
+        private const string FolderPath = "..\\..\\..\\Resources\\";
+
+        static void Main()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Console.WriteLine("Please enter name of the file (without .txt)");
+
+            var name = Console.ReadLine();
+
+            if (name is null) throw new ArgumentNullException();
+
+            var path = Path.Combine(FolderPath, name + ".txt");
+            var resultFile = Path.Combine(FolderPath, "Result.txt");
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Place your file in the resources folder");
+            }
+            var allText = File.ReadAllText(path, Encoding.GetEncoding(1251));
+
+            var stopwatch = new Stopwatch();
+            var stopwatch2 = new Stopwatch();
+
+
+            var textProcessor = new TextProcessor.TextProcessor();
+
+            var type = textProcessor.GetType();
+
+            var methodInfo = type.GetMethod("Run", BindingFlags.NonPublic | BindingFlags.Instance);
+
+
+            //Time measurement
+
+            stopwatch.Start();
+
+            var result = (IEnumerable<string>)methodInfo!.Invoke(textProcessor, parameters: new object?[] { allText })!;
+
+            stopwatch.Stop();
+
+            stopwatch2.Start();
+
+            textProcessor.RunParallel(allText);
+
+            stopwatch2.Stop();
+
+
+            Console.WriteLine($"Default method execution time: {stopwatch.ElapsedMilliseconds } milliseconds");
+            Console.WriteLine($"Parallel method execution time: {stopwatch2.ElapsedMilliseconds } milliseconds");
+
+
+            File.WriteAllLines(resultFile, result);
+
+        }
+    }
+}
---
+++ TextAnalyzer/SecondTask/TextProcessorDLL.ForSecondTask.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SecondTask
+{
+    public class TextProcessorDLL
+    {
+        private const string FolderPath = "..\\..\\..\\Resources\\";
+
+        public IEnumerable<string> RunParallel(string allText)
+        {
+
+            var resultFile = Path.Combine(FolderPath, "Result.txt");
+
+            string[] splitWords = new Regex("[^a-zA-Zа-яА-Я]|\\b[a-zA-Zа-яА-Яё]{1,3}\\b").Replace(allText, " ").ToLower().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var uniqueWords = splitWords.AsParallel()
+                .GroupBy(x => x).OrderByDescending(x => x.Count());
+
+            return uniqueWords.Select(gr => $"{gr.Key} {gr.Count()}");
+
+        }
+
+        private IEnumerable<string> Run(string allText)
+        {
+
+            var resultFile = Path.Combine(FolderPath, "Result.txt");
+
+            string[] splitWords = new Regex("[^a-zA-Zа-яА-Я]|\\b[a-zA-Zа-яА-Яё]{1,3}\\b").Replace(allText, " ").ToLower().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var uniqueWords = splitWords.GroupBy(x => x).OrderByDescending(x => x.Count());
+
+            return uniqueWords.Select(gr => $"{gr.Key} {gr.Count()}");
+
+        }
+
+    }
+}
