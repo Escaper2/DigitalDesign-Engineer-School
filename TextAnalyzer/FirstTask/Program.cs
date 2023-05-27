d9e8fbc3d30abab637d11d696aa58db9f60df9fd
Eskaper2 <vlad81552@gmail.com>
2023-05-27 19:34:57 +0300

    feat: first task

---
+++ TextAnalyzer/FirstTask/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using System.Text;
+using TextProcessor;
+
+namespace FirstTask
+{
+    internal class Program
+    {
+
+        private const string FolderPath = "..\\..\\..\\Resources\\";
+
+        static void Main()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Console.WriteLine("Please enter name of the file (without .txt)");
+
+            var name = Console.ReadLine();
+
+            if (name is null) throw new ArgumentNullException();
+
+            var path = Path.Combine(FolderPath, name + ".txt");
+            var resultFile = Path.Combine(FolderPath, "Result.txt");
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Place your file in the resources folder");
+            }
+            var allText = File.ReadAllText(path, Encoding.GetEncoding(1251));
+
+
+
+            var textProcessor = new TextProcessor.TextProcessor();
+
+            var type = textProcessor.GetType();
+
+            var methodInfo = type.GetMethod("Run", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var result = (IEnumerable<string>)methodInfo!.Invoke(textProcessor, parameters: new object?[] { allText })!;
+
+            File.WriteAllLines(resultFile, result);
+
+        }
+    }
+}
