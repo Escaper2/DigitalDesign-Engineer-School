dbac93e8b5511714e960e6d56f1d6225526cd7e2
Eskaper2 <vlad81552@gmail.com>
2023-06-03 03:07:48 +0300

    feat: add second task for frontend block

---
+++ DigitalDesign.Frontend/SecondTask/main.js
@@ -0,0 +1,29 @@
+const url = 'https://www.cbr-xml-daily.ru/daily_json.js';
+const button = document.getElementById('get-data');
+const content = document.getElementById('content');
+
+async function getData() {
+    const response = await fetch(url);
+    return await response.json();
+}
+
+async function parseData() {
+    const data = await getData();
+    const valutes = data["Valute"];
+    console.log(valutes);
+    for (const valute in valutes) {
+        const newElement = document.createElement('div');
+        if (valute === "USD" || valute === "EUR") {
+            newElement.innerHTML = `${valutes[valute]["Name"]}:  - ${valutes[valute]["Value"]} рублей`;
+            content.append(newElement)
+        }
+    }
+}
+
+async function onClick() {
+    button.removeEventListener('click', onClick);
+    const data = await getData();
+    await parseData(data);
+}
+
+button.addEventListener('click', onClick);
